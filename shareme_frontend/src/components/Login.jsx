7d4a980650df2bbf63e33fbf7fd8aaf32495a472
enochadane <henokadane8@gmail.com>
2022-11-24 10:41:55 +0300

    Social Media app react fontend and sanity backend

---
+++ shareme_frontend/src/components/Login.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+// import GoogleLogin from "react-google-login";
+import {
+  GoogleOAuthProvider,
+  GoogleLogin,
+  useGoogleOneTapLogin,
+} from "@react-oauth/google";
+import jwt_decode from "jwt-decode";
+import { useNavigate } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+import { v4 as uuidv4 } from "uuid";
+
+import shareVideo from "../assets/share.mp4";
+import logo from "../assets/logowhite.png";
+
+import { client } from "../client";
+
+const Login = () => {
+  const navigate = useNavigate();
+
+  const responseGoogle = (response) => {
+    const decoded = jwt_decode(response.credential);
+
+    localStorage.setItem("user", JSON.stringify(decoded));
+
+    const { clientId } = response;
+    const {sub ,name, picture } = decoded;
+
+    const doc = {
+      _id: sub,
+      _type: "user",
+      userName: name,
+      image: picture,
+    };
+
+    client.createIfNotExists(doc).then(() => {
+      navigate("/", { replace: true });
+    });
+  };
+
+  return (
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className="relative w-full h-full">
+        <video
+          src={shareVideo}
+          type="video/mp4"
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className="w-full h-full object-cover"
+        />
+
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+          <div className="p-5">
+            <img src={logo} width="130px" alt="logo" />
+          </div>
+
+          <div className="shadow-2xl">
+            <GoogleOAuthProvider
+              clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
+            >
+              <GoogleLogin
+                onSuccess={responseGoogle}
+                onError={responseGoogle}
+                useOneTap
+              />
+              ;
+            </GoogleOAuthProvider>
+            ;
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
